174fd7f661bb49f60dd1e365cf4f9a012e921609
onesunny2 <ippne996@gmail.com>
2025-01-18 01:55:43 +0900

    [design] BaseView 구현

---
+++ UnsplashImage/BaseView/BaseView.swift
@@ -0,0 +1,31 @@
+//
+//  BaseView.swift
+//  UnsplashImage
+//
+//  Created by Lee Wonsun on 1/18/25.
+//
+
+import UIKit
+
+class BaseView: UIView {
+    
+    // ❔ 모든 뷰에서 navigationController가 사용될 것 같아 여기에 심어뒀는데 괜찮을까요?
+    let navigationBar = UINavigationBar()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configHierarchy()
+        configLayout()
+        configView()
+    }
+    
+    func configHierarchy() {}
+    func configLayout() {}
+    func configView() {}
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
