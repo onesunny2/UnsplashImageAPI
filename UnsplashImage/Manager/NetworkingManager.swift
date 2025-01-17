4a24d2d2f14d0adca757ea0c82b92555559c20ae
onesunny2 <ippne996@gmail.com>
2025-01-17 15:48:43 +0900

    add: .config gitignore

---
+++ UnsplashImage/Manager/NetworkingManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkingManager.swift
+//  UnsplashImage
+//
+//  Created by Lee Wonsun on 1/17/25.
+//
+
+import Foundation
---
+++ UnsplashImage/SearchPhoto/Button/FilterButton.swift
@@ -0,0 +1,8 @@
+//
+//  FilterButton.swift
+//  UnsplashImage
+//
+//  Created by Lee Wonsun on 1/17/25.
+//
+
+import Foundation
