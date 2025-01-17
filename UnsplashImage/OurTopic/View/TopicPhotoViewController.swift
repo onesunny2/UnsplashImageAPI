392537ce6c8ee0db3f5429c1c06f6ca4e275a900
onesunny2 <ippne996@gmail.com>
2025-01-17 16:44:07 +0900

    [feat] Main 스토리보드 삭제 및 TabBarController 구현

---
+++ UnsplashImage/OurTopic/View/TopicPhotoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TopicPhotoViewController.swift
+//  UnsplashImage
+//
+//  Created by Lee Wonsun on 1/17/25.
+//
+
+import UIKit
+
+class TopicPhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+
+
+}
---
+++ UnsplashImage/TabBar/TabBarController.swift
@@ -0,0 +1,59 @@
+//
+//  TabBarController.swift
+//  UnsplashImage
+//
+//  Created by Lee Wonsun on 1/17/25.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureTabBarController()
+        setupTabBarAppearance()
+    }
+    
+    func configureTabBarController() {
+        let firstVc = TopicPhotoViewController()
+        firstVc.tabBarItem.title = ""
+        firstVc.tabBarItem.image = UIImage(systemName: "folder")
+        firstVc.tabBarItem.selectedImage = UIImage(systemName: "folder.fill")
+        
+        
+        let secondVc = SecondViewController()
+        secondVc.tabBarItem.title = ""
+        secondVc.tabBarItem.image = UIImage(systemName: "star")
+        secondVc.tabBarItem.selectedImage = UIImage(systemName: "star.fill")
+        
+        let thirdVc = SearchPhotoViewController()
+        thirdVc.tabBarItem.title = ""
+        thirdVc.tabBarItem.image = UIImage(systemName: "magnifyingglass.circle")
+        thirdVc.tabBarItem.selectedImage = UIImage(systemName: "magnifyingglass.circle.fill")
+        
+        let fourthVc = LikePhotoViewController()
+        fourthVc.tabBarItem.title = ""
+        fourthVc.tabBarItem.image = UIImage(systemName: "heart")
+        fourthVc.tabBarItem.selectedImage = UIImage(systemName: "heart.fill")
+        
+        setViewControllers([firstVc, secondVc, thirdVc, fourthVc], animated: true)
+    }
+    
+    func setupTabBarAppearance() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundColor = .systemBackground
+        tabBar.standardAppearance = appearance
+        tabBar.scrollEdgeAppearance = appearance
+        tabBar.tintColor = .label
+    }
+}
+
+extension TabBarController: UITabBarControllerDelegate {
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        print(item)
+    }
+}
